ee0210fc3738d782337d3566f2d47153eb5ecf03
Felipe <f.alfonso@res-ear.ch>
2023-07-14 15:19:47 -0400

    updates and fixes

--- Formula/term-notes.rb
+++ Formula/term-notes.rb
@@ -4,16 +4,35 @@ class TermNotes < Formula
   url "https://github.com/felipealfonsog/TermNotes/archive/refs/tags/v.1.0.1.tar.gz"
   sha256 "43db1b333d8902b15df4444d12e76c6d80fa9535cb377f7594d3cfa19c64e869"
   license "MIT"
-  depends_on "gcc" => :build
+
+  depends_on "gcc"
   depends_on "vim"
   depends_on "nano"
 
   def install
-    system "./setup"
-    bin.install "term_notes"
+    if OS.mac?
+      bin.install "term_notes" => "term-notes"
+    else
+      bin.install "term_notes"
+    end
+  end
+
+  def caveats
+    <<~EOS
+      'Term Notes' has been installed!
+
+      On Mac OS, you can run the program using 'term-notes'.
+
+      On Linux, you may need to create a symbolic link to make it accessible globally.
+      Run the following command:
+
+        sudo ln -s #{opt_bin}/term_notes /usr/local/bin/term-notes
+
+      Enjoy taking notes with 'Term Notes'!
+    EOS
   end
 
   test do
-    # Add some test cases here
+    # Add your test logic here
   end
 end
